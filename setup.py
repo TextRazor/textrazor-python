21c119353a9d9efebea2da36cc2336eda5b58360
Toby Crayston <toby@textrazor.com>
2016-02-08 15:20:59 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='textrazor',
-    version='1.2.1',
+    version='1.2.2',
     description='Official Python SDK for TextRazor (https://textrazor.com).',
     long_description=open('README.rst').read(),
     author='TextRazor Ltd.',
